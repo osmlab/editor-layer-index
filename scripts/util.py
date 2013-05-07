27abd2f0ead1db12e22e5467b736cd2139850dc6
John Firebaugh <john.firebaugh@gmail.com>
2013-05-07 15:50:27 -0700

    Reverse build direction
    Convert individual JSON files to XML and concatenated (Geo)JSON
    files, rather than the other way around.
    
    Refs #8.

--- scripts/util.py
+++ scripts/util.py
@@ -33,3 +33,18 @@ def getrings(elem):
             rings.append(ring)
         return bbox, rings
     return None, None
+
+def indent(elem, level=0):
+    i = "\n" + level*"  "
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + "  "
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+        for elem in elem:
+            indent(elem, level+1)
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = i
