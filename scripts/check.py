d584a9a32a5f765a8c898e36af7620cc70a43816
Bryan Housel <bryan@mapbox.com>
2016-11-11 09:40:30 -0500

    Add progress dots to check.py

--- scripts/check.py
+++ scripts/check.py
@@ -7,6 +7,10 @@ for file in sys.argv[1:]:
     source = json.load(io.open(file, encoding='utf-8'))
     try:
         validate(source, schema)
+        sys.stdout.write('.')
+        sys.stdout.flush()
     except ValidationError as e:
         print(file)
         raise
+
+print('')
