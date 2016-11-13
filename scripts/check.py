97084aadbf18411663bd852a5310d773d2fb756a
Simon Legner <Simon.Legner@gmail.com>
2016-11-13 12:28:10 +0100

    Check that ids are unique

--- scripts/check.py
+++ scripts/check.py
@@ -2,11 +2,16 @@ import json, sys, io
 from jsonschema import validate, ValidationError
 
 schema = json.load(io.open('schema.json', encoding='utf-8'))
+seen_ids = set()
 
 for file in sys.argv[1:]:
     source = json.load(io.open(file, encoding='utf-8'))
     try:
         validate(source, schema)
+        id = source['properties']['id']
+        if id in seen_ids:
+            raise ValidationError('Id %s used multiple times' % id)
+        seen_ids.add(id)
         sys.stdout.write('.')
         sys.stdout.flush()
     except ValidationError as e:
