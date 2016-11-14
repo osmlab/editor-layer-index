724dbfc9804fa0a452ba857c4f05d253084c858e
simonpoole <simon@poole.ch>
2016-11-14 22:18:54 +0100

    Add generated bbox to legacy output

--- scripts/convert_geojson_to_legacyjson.py
+++ scripts/convert_geojson_to_legacyjson.py
@@ -8,6 +8,27 @@ def convert_json_source(source):
     polygon_coords = geometry.get('coordinates') or []
     if polygon_coords:
         extent_obj['polygon'] = polygon_coords
+        # generate bbox from polygon coordinates as a stop gap         
+        min_lon = 180
+        max_lon = -180
+        min_lat = 90
+        max_lat = -90
+        for ring in polygon_coords:
+            for coord in ring:
+                if coord[0] < min_lon:
+                    min_lon = coord[0]
+                if coord[0] > max_lon:
+                    max_lon = coord[0]
+                if coord[1] < min_lat:
+                    min_lat = coord[1]
+                if coord[1] > max_lat:
+                    max_lat = coord[1]
+        bbox_obj = {}
+        bbox_obj['min_lon'] = min_lon
+        bbox_obj['max_lon'] = max_lon
+        bbox_obj['min_lat'] = min_lat
+        bbox_obj['max_lat'] = max_lat
+        extent_obj['bbox'] = bbox_obj
 
     properties = source.get('properties') or {}
     for f in ['name', 'type', 'url', 'license_url', 'id', 'description',
