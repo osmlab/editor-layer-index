68f205c9f3976090e9d9be97ac5342b7cb45159f
simonpoole <simon@poole.ch>
2018-01-18 23:46:52 +0100

    Add a timestamp when the file was generated and a version string

--- scripts/concat_geojson.py
+++ scripts/concat_geojson.py
@@ -1,4 +1,5 @@
 import json, sys, util, io
+from datetime import datetime
 from xml.dom.minidom import parse
 
 source_features = []
@@ -6,8 +7,15 @@ for file in sys.argv[1:]:
     with io.open(file, 'r') as f:
         source_features.append(json.load(f))
 
+generated = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.utcnow())
+version = "1.0"
+
 collection = {
     "type": "FeatureCollection",
+    "meta": {
+        "generated": generated,
+        "format_version": version
+    },
     "features": source_features
 }
 
