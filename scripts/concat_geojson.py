38d9e0c1da3c886445fa26da2bd1f99c7b693f41
Guillaume RISCHARD <git@stereo.lu>
2018-05-30 13:13:41 +0200

    simplify all floats to 5 decimal points in all scripts

--- scripts/concat_geojson.py
+++ scripts/concat_geojson.py
@@ -7,7 +7,7 @@ from datetime import datetime
 source_features = []
 for file in sys.argv[1:]:
     with io.open(file, 'r') as f:
-        # simplify all floats to 6 decimal points
+        # simplify all floats to 5 decimal points
         source_features.append(json.load(f, parse_float=lambda x: round(float(x), 5)))
 
 generated = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.utcnow())
