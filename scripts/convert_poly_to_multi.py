095ebe0dd63e69e9dfc671eea4b23c57a5f8b898
René Buffat <buffat@gmail.com>
2020-07-21 18:50:06 +0200

    script to convert non valid geometries

---
+++ scripts/convert_poly_to_multi.py
@@ -0,0 +1,39 @@
+import io
+import json
+import glob
+
+from jsonschema import ValidationError
+from shapely.geometry import shape, mapping, Polygon, MultiPolygon
+from shapely.validation import explain_validity, make_valid
+
+
+def dict_raise_on_duplicates(ordered_pairs):
+    """Reject duplicate keys."""
+    d = {}
+    for k, v in ordered_pairs:
+        if k in d:
+            raise ValidationError("duplicate key: %r" % (k,))
+        else:
+            d[k] = v
+    return d
+
+
+for filename in glob.glob('../sources/**/*.geojson', recursive=True):
+
+    source = json.load(io.open(filename, encoding='utf-8'), object_pairs_hook=dict_raise_on_duplicates)
+
+    if source['geometry'] is None:
+        continue
+    geom = shape(source['geometry'])
+
+    if not geom.is_valid:
+        print("Not valid: {}".format(filename))
+        reason = explain_validity(geom)
+        print("Reason: {}".format(reason))
+        geom = make_valid(geom)
+
+        if isinstance(geom, Polygon) or isinstance(geom, MultiPolygon):
+            source['geometry'] = mapping(geom)
+            print("Overwrite: {}".format(filename))
+            with open(filename, 'w') as f:
+                json.dump(source, f, ensure_ascii=False, separators=(',', ':'), indent=4, sort_keys=True)
