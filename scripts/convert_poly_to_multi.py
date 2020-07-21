f79948442156330fc335bd2df60083b4ebd0a9b8
René Buffat <buffat@gmail.com>
2020-07-21 21:23:46 +0200

    update stript to only keep polygons and multipolygons

--- scripts/convert_poly_to_multi.py
+++ scripts/convert_poly_to_multi.py
@@ -3,7 +3,8 @@ import json
 import glob
 
 from jsonschema import ValidationError
-from shapely.geometry import shape, mapping, Polygon, MultiPolygon
+from shapely.geometry import shape, mapping, Polygon, MultiPolygon, GeometryCollection
+from shapely.ops import cascaded_union
 from shapely.validation import explain_validity, make_valid
 
 
@@ -32,8 +33,19 @@ for filename in glob.glob('../sources/**/*.geojson', recursive=True):
         print("Reason: {}".format(reason))
         geom = make_valid(geom)
 
+        # Keep only polygons and multipolygons
+        if isinstance(geom, GeometryCollection):
+            keep = []
+            for g in geom.geoms:
+                if isinstance(g, Polygon) or isinstance(g, MultiPolygon):
+                    keep.append(g)
+            geom = cascaded_union(keep)
+
         if isinstance(geom, Polygon) or isinstance(geom, MultiPolygon):
             source['geometry'] = mapping(geom)
             print("Overwrite: {}".format(filename))
             with open(filename, 'w') as f:
                 json.dump(source, f, ensure_ascii=False, separators=(',', ':'), indent=4, sort_keys=True)
+
+        else:
+            print("No transformation is possible")
