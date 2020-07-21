c6cc5cb5cb6f43b56f42437d43583c9fae8db39b
René Buffat <buffat@gmail.com>
2020-07-21 20:46:05 +0200

    remove skipstrict

--- scripts/check.py
+++ scripts/check.py
@@ -65,12 +65,6 @@ spacesave = 0
 
 strict_mode = arguments.strict
 
-# Disable strict mode if @skipstrict is in the commit message
-if "@skipstrict" in os.environ['TRAVIS_COMMIT_MESSAGE']:
-    strict_mode = False
-
-print(os.environ['TRAVIS_COMMIT_MESSAGE'], "@skipstrict" in os.environ['TRAVIS_COMMIT_MESSAGE'])
-
 headers = {'User-Agent': 'Mozilla/5.0 (compatible; MSIE 6.0; OpenStreetMap Editor Layer Index CI check)'}
 
 for filename in arguments.path:
