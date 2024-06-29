4f02fd6ec6e500a9edc929b9bc09dd545e4906db
Intrinsical <git@intrinsical.de>
2024-06-29 10:35:40 -0400

    almost at basic functionality (just missing replacing icon URL in the GeoJSON)

--- scripts/unembed_icons.py
+++ scripts/unembed_icons.py
@@ -4,8 +4,11 @@ import mimetypes
 from pathlib import Path
 import json
 import logging
+import tkinter.filedialog as fd
 
-root = None
+host = "https://osmlab.github.io/editor-layer-index/"
+
+root_path = None
 
 def getData(url): # returns: [mime, data] or None upon error
 	data_re = re.search(r"^data:(.*);base64,(.*)$", url)
@@ -32,7 +35,7 @@ def decodeLines(data):
 		logging.debug("data is binary")
 		return None
 
-def findFile(parent_path, data, mime): # returns: Path or None upon none found
+def findFile(parent_path, mime, data): # returns: Path or None upon none found
 	# NOTE: all the extra stuff with text data is to compensate for git changing newlines
 	
 	data_size = len(data)
@@ -47,7 +50,7 @@ def findFile(parent_path, data, mime): # returns: Path or None upon none found
 	if len(extensions) == 1:
 		glob = "*{}".format(extensions[0])
 	elif len(extensions) > 1:
-		glob = "*[{}]".format("][".join(extensions))
+		glob = "*[{}]".format("][".join(extensions)) # IMPROVEMENT: separate out the dot, instead of having all.
 	else:
 		logging.warning("invalid mime-type")
 		return None # TODO: remove when `TODO: force only match files (not including directories)' is done
@@ -55,7 +58,7 @@ def findFile(parent_path, data, mime): # returns: Path or None upon none found
 	
 	logging.info("walking `{}' with glob: `{}'".format(parent_path, glob))
 	check_count = 0
-	for file_path in parent_path.rglob(glob): # OPTIMIZE: ignore hidden files (especially .git) # TODO: force only match files (not including directories)
+	for file_path in parent_path.rglob(glob): # TODO: ignore hidden files (especially .git) and only match files (i.e. not including directories)
 		check_count += 1
 		logging.debug("checking against file: `{}'".format(file_path))
 		file = None
@@ -82,9 +85,59 @@ def findFile(parent_path, data, mime): # returns: Path or None upon none found
 	logging.warning("no match found in {} checked files".format(check_count))
 	return None
 
+def saveAs(directory, mime):
+	exts = mimetypes.guess_all_extensions(mime, strict=True)
+	exts_mod = []
+	for ext in exts:
+		exts_mod.append((ext[1:].upper(), ext))
+	logging.info("waiting for Save As dialog")
+	output_path = Path(fd.asksaveasfilename(filetypes=exts_mod, defaultextension=exts_mod[0], initialdir=directory))
+	# TODO: backup input for terminal interfaces (dumb input(); check if parent is at least valid; relative to repo root_path; list recommended extensions; make sure the import failure is dealt with)
+	
+	return output_path
+
+def save(path, binary, data):
+	# TODO: confirm with user first, and show if it will be replacing something, and of course honor command line arguments
+	mode = None # TODO: maybe just require input to be binary, actually?
+	if binary == True:
+		mode = "wb"
+		mode_str = "bytes"
+	elif binary == False:
+		mode = "w"
+		mode_str = "chars"
+	file_handle = open(path, mode)
+	logging.info("saving {} {} into the file `{}'".format(len(data), mode_str, path))
+	file = file_handle.write(data)
+	file_handle.close()
+
+def single(geojson_path):
+	geojson_handle = open(geojson_path)
+	geojson = json.load(geojson_handle)
+	geojson_handle.close()
+	
+	url = geojson["properties"]["icon"]
+	
+	url_mime, url_data = getData(url) # TODO: gracefully quit if it returns None
+	
+	icon_path = findFile(root_path, url_mime, url_data)
+	if icon_path == None:
+		icon_path = saveAs(geojson_path.parent, url_mime) # TODO: handle cancel
+		save(icon_path, True, url_data)
+	new_url = host + icon_path.relative_to(root_path).as_posix() # TODO: make sure it is underneath
+	logging.info("new URL: `{}'".format(new_url))
+	# TODO: handle replacing url in the GeoJSON: do a search and replace of the icon url as to preserve formatting of file (check it can find it at the beginning, to present a warning to the user (that it wont be able to replace)). (yes I know this method could cause issues normally, because escaped chars, but data URLs shouldn't have that)
+
 def main():
-	pass # TODO
+	logging.getLogger().setLevel(logging.DEBUG)
+	
+	geojson_path = fd.askopenfilename(filetypes=[("GeoJSON", ".geojson")], initialdir=root_path / "sources")
+	single(Path(geojson_path))
+	
+	# TODO: handle more than one GeoJSON (should take in a folder too (rglob that), and multiple files, or a mix of both)
+	# OPTIMIZE: use a cache of hashes if doing multiple files
+	# TODO: posix interface with these options: --confirm-all --no-confirm-overwrite --only-existing + log level stuff
+	# TODO: maybe duplicate the old version's interface as much as possible?
 
 if __name__ == "__main__":
-	root = Path(__file__).parents[1] # get repository's root
+	root_path = Path(__file__).parents[1] # get repository's root_path
 	main()
