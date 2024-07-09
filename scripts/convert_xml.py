12cc929ce111f9f6c25ee583584e81554df0eb83
Simon Poole <simon@poole.ch>
2024-07-09 10:07:23 +0200

    Skip "ZZ" (unknown) country code

--- scripts/convert_xml.py
+++ scripts/convert_xml.py
@@ -73,7 +73,7 @@ def add_source(source):
         icon = ET.SubElement(entry, "icon")
         icon.text = props["icon"]
 
-    if "country_code" in props:
+    if "country_code" in props and props["country_code"].upper() != "ZZ":
         country_code = ET.SubElement(entry, "country-code")
         country_code.text = props["country_code"]
 
